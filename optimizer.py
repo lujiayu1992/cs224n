ebc6235aad304d4aa33c003e9a93109505496464
Lucas Lu <lujiayu6@gmail.com>
2025-06-15 16:19:50 -0700

    Adam optimizer

--- optimizer.py
+++ optimizer.py
@@ -61,7 +61,16 @@ class AdamW(Optimizer):
                 ###
                 ###       Refer to the default project handout for more details.
                 ### YOUR CODE HERE
-                raise NotImplementedError
-
-
+                betas, eps, weight_decay = group["betas"], group["eps"], group["weight_decay"]
+                beta_one, beta_two = betas
+                if len(state) ==0:
+                    state["m"] = torch.zeros_like(p.data)
+                    state["v"] = torch.zeros_like(p.data)
+                    state['t'] = 0
+                state['t'] +=1
+                state["m"] = beta_one*state["m"] + (1-beta_one)*grad
+                state["v"] = beta_two*state["v"] +(1-beta_two)*grad**2
+                alpha_t = alpha*(1-beta_two**state['t'])**.5/(1-beta_one**state['t'])
+                p.data -= alpha_t*state["m"]/(state["v"]**.5 +eps)
+                p.data *= 1-weight_decay*alpha
         return loss
